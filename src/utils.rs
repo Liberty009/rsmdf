17be2762dccdf2c96d3f6a74af5094594d9c1de8
Jack <daadaa199@hotmail.co.uk>
2021-11-03 21:29:33 +0000

    Add generics for conversion

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,8 @@
 use std::mem;
 
+use byte_slice_cast::FromByteSlice;
 use byteorder::{BigEndian, ByteOrder, LittleEndian};
+use num_traits::int::PrimInt;
 use xml::reader::{EventReader, XmlEvent};
 
 pub fn extract_name(text: &[u8]) -> String {
@@ -39,9 +41,143 @@ pub fn extract_name(text: &[u8]) -> String {
     return display_name;
 }
 
+pub trait FromBytes {
+    fn from_be_bytes(a: &[u8]) -> Self;
+    fn from_le_bytes(a: &[u8]) -> Self;
+}
+
+impl<const N: usize> FromBytes for [u8; N] {
+    fn from_be_bytes(a: &[u8]) -> [u8; N] {
+        let (int_bytes, _rest) = a.split_at(N);
+
+        let mut me = [0u8; N];
+        me.copy_from_slice(int_bytes);
+
+        //*a = rest;
+        me
+    }
+    fn from_le_bytes(a: &[u8]) -> [u8; N] {
+        let (int_bytes, _rest) = a.split_at(N);
+
+        let mut me = [0u8; N];
+        me.copy_from_slice(int_bytes);
+
+        //*a = rest;
+        me
+    }
+}
+
+impl FromBytes for u64 {
+    fn from_be_bytes(a: &[u8]) -> Self {
+        Self::from_be_bytes(FromBytes::from_be_bytes(a))
+    }
+    fn from_le_bytes(a: &[u8]) -> Self {
+        Self::from_le_bytes(FromBytes::from_le_bytes(a))
+    }
+}
+impl FromBytes for u32 {
+    fn from_be_bytes(a: &[u8]) -> Self {
+        Self::from_be_bytes(FromBytes::from_be_bytes(a))
+    }
+    fn from_le_bytes(a: &[u8]) -> Self {
+        Self::from_le_bytes(FromBytes::from_le_bytes(a))
+    }
+}
+impl FromBytes for u16 {
+    fn from_be_bytes(a: &[u8]) -> Self {
+        Self::from_be_bytes(FromBytes::from_be_bytes(a))
+    }
+    fn from_le_bytes(a: &[u8]) -> Self {
+        Self::from_le_bytes(FromBytes::from_le_bytes(a))
+    }
+}
+impl FromBytes for u8 {
+    fn from_be_bytes(a: &[u8]) -> Self {
+        Self::from_be_bytes(FromBytes::from_be_bytes(a))
+    }
+    fn from_le_bytes(a: &[u8]) -> Self {
+        Self::from_le_bytes(FromBytes::from_le_bytes(a))
+    }
+}
+
+impl FromBytes for i64 {
+    fn from_be_bytes(a: &[u8]) -> Self {
+        Self::from_be_bytes(FromBytes::from_be_bytes(a))
+    }
+    fn from_le_bytes(a: &[u8]) -> Self {
+        Self::from_le_bytes(FromBytes::from_le_bytes(a))
+    }
+}
+impl FromBytes for i32 {
+    fn from_be_bytes(a: &[u8]) -> Self {
+        Self::from_be_bytes(FromBytes::from_be_bytes(a))
+    }
+    fn from_le_bytes(a: &[u8]) -> Self {
+        Self::from_le_bytes(FromBytes::from_le_bytes(a))
+    }
+}
+impl FromBytes for i16 {
+    fn from_be_bytes(a: &[u8]) -> Self {
+        Self::from_be_bytes(FromBytes::from_be_bytes(a))
+    }
+    fn from_le_bytes(a: &[u8]) -> Self {
+        Self::from_le_bytes(FromBytes::from_le_bytes(a))
+    }
+}
+
+impl FromBytes for i8 {
+    fn from_be_bytes(a: &[u8]) -> Self {
+        Self::from_be_bytes(FromBytes::from_be_bytes(a))
+    }
+    fn from_le_bytes(a: &[u8]) -> Self {
+        Self::from_le_bytes(FromBytes::from_le_bytes(a))
+    }
+}
+
+impl FromBytes for f64 {
+    fn from_be_bytes(a: &[u8]) -> Self {
+        f64::from_be_bytes(FromBytes::from_be_bytes(a))
+    }
+    fn from_le_bytes(a: &[u8]) -> Self {
+        Self::from_le_bytes(FromBytes::from_le_bytes(a))
+    }
+}
+
+pub fn read_be<T: FromBytes>(input: &[u8]) -> T {
+    T::from_be_bytes(input)
+}
+pub fn read_le<T: FromBytes>(input: &[u8]) -> T {
+    T::from_le_bytes(input)
+}
+
+// pub fn read<T>(stream: &[u8], _little_endian: bool, position: &mut usize) -> [T]
+// where T: Copy + FromByteSlice
+// {
+
+// 	*position += mem::size_of::<T>() / mem::size_of::<u8>();
+//     // if little_endian {
+//     //     return LittleEndian::read::<T>(stream);
+//     // } else {
+//     //     return BigEndian::read::<T>>(stream);
+//     // }
+
+// 	let values = T::from_byte_slice(&stream).expect("");
+
+// 	// if little_endian {
+// 		return values;
+// 	// } else {
+// 	// 	return T::from_be_bytes(stream).expect("");
+// 	// }
+// }
+
 pub fn read_u8(stream: &[u8], _little_endian: bool, position: &mut usize) -> u8 {
     *position += 1;
     return stream[0];
+}
+
+pub fn read_i8(stream: &[u8], _little_endian: bool, position: &mut usize) -> i8 {
+    *position += 1;
+    return unsafe { std::mem::transmute::<u8, i8>(stream[0]) };
 }
 
 pub fn read_u16(stream: &[u8], little_endian: bool, position: &mut usize) -> u16 {
