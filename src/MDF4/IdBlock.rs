8e46fbdde13680caab3ae7b1d97aabcc11e08751
Jack <daadaa199@hotmail.co.uk>
2022-04-12 15:03:47 +0100

    Add MDF4 ID Block test

--- src/MDF4/IdBlock.rs
+++ src/MDF4/IdBlock.rs
@@ -9,11 +9,12 @@ pub struct Idblock {
     id_vers: [u8; 8],
     #[allow(dead_code)]
     id_prog: [u8; 8],
-
-    //id_reserved1: [u8; 4],
+	#[allow(dead_code)]
+    id_reserved1: [u8; 4],
     #[allow(dead_code)]
     id_ver: u16,
-    //id_reserved2: [u8; 34],
+	#[allow(dead_code)]
+    id_reserved2: [u8; 34],
 }
 impl Block for Idblock {
     fn new() -> Self {
@@ -21,9 +22,9 @@ impl Block for Idblock {
             id_file: [0; 8],
             id_vers: [0; 8],
             id_prog: [0; 8],
-            //id_reserved1: [0; 4],
+            id_reserved1: [0; 4],
             id_ver: 0,
-            //id_reserved2: [0; 34],
+            id_reserved2: [0; 34],
         }
     }
     fn default() -> Self {
@@ -31,9 +32,9 @@ impl Block for Idblock {
             id_file: [0; 8],
             id_vers: [0; 8],
             id_prog: [0; 8],
-            //id_reserved1: [0; 4],
+            id_reserved1: [0; 4],
             id_ver: 0,
-            //id_reserved2: [0; 34],
+            id_reserved2: [0; 34],
         }
     }
     fn read(stream: &[u8], _position: usize, _little_endian: bool) -> (usize, Self) {
@@ -42,9 +43,9 @@ impl Block for Idblock {
         let id_file = utils::read(stream, _little_endian, &mut pos);
         let id_vers = utils::read(stream, litte_endian, &mut pos);
         let id_prog = utils::read(stream, litte_endian, &mut pos);
-        let _id_reserved1: [u8; 4] = utils::read(stream, litte_endian, &mut pos);
+        let id_reserved1: [u8; 4] = utils::read(stream, litte_endian, &mut pos);
         let id_ver = utils::read(stream, litte_endian, &mut pos);
-        let _id_reserved2: [u8; 34] = utils::read(stream, litte_endian, &mut pos);
+        let id_reserved2: [u8; 34] = utils::read(stream, litte_endian, &mut pos);
 
         (
             pos,
@@ -52,10 +53,31 @@ impl Block for Idblock {
                 id_file,
                 id_vers,
                 id_prog,
-                //id_reserved1,
+                id_reserved1,
                 id_ver,
-                //id_reserved2,
+                id_reserved2,
             },
         )
     }
+}
+
+#[test]
+fn id_read_test() {
+    let raw: [u8; 64] = [
+        0x4D, 0x44, 0x46, 0x20, 0x20, 0x20, 0x20, 0x20, 0x34, 0x2E, 0x31, 0x30, 0x20, 0x20, 0x20,
+        0x20, 0x54, 0x47, 0x54, 0x20, 0x31, 0x35, 0x2E, 0x30, 0x00, 0x00, 0x00, 0x00, 0x9A, 0x01,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00,
+    ];
+
+    let (pos, id_result) = Idblock::read(&raw, 0, true);
+
+    assert_eq!(64, pos);
+	assert!(utils::eq("MDF     ".as_bytes(), &id_result.id_file));
+	assert!(utils::eq("4.10    ".as_bytes(), &id_result.id_vers));
+	assert!(utils::eq("TGT 15.0".as_bytes(), &id_result.id_prog));
+	assert!(utils::eq(&[0_u8; 4], &id_result.id_reserved1));
+	assert_eq!(410, id_result.id_ver);
+	assert!(utils::eq(&[0_u8; 34], &id_result.id_reserved2));
 }
