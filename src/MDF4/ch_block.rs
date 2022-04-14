3bd749b764f5a7964c7a7b32d235bc21d6abffbd
Jack <daadaa199@hotmail.co.uk>
2022-04-14 11:02:08 +0100

    Update ch_block.rs
    Add byte_len function

--- src/MDF4/ch_block.rs
+++ src/MDF4/ch_block.rs
@@ -1,26 +1,20 @@
+use std::mem;
+
 use crate::utils;
 
-use super::block_header::*;
 use super::block::Block;
+use super::block_header::*;
 use super::mdf4::link_extract;
 use super::mdf4_enums::ChannelHierarchyType;
 
-
 pub struct Chblock {
     header: BlockHeader,
-    #[allow(dead_code)]
     ch_ch_next: u64,
-    #[allow(dead_code)]
     ch_ch_first: u64,
-    #[allow(dead_code)]
     ch_tx_name: u64,
-    #[allow(dead_code)]
     ch_md_comment: u64,
-    #[allow(dead_code)]
     ch_element: Vec<u64>,
-    #[allow(dead_code)]
     ch_element_count: u32,
-    #[allow(dead_code)]
     ch_type: ChannelHierarchyType,
 }
 impl Block for Chblock {
@@ -85,6 +79,17 @@ impl Block for Chblock {
     }
 
     fn byte_len(&self) -> usize {
-        todo!()
+        let mut length = self.header.byte_len()
+            + mem::size_of_val(&self.ch_ch_next)
+            + mem::size_of_val(&self.ch_ch_first)
+            + mem::size_of_val(&self.ch_tx_name)
+            + mem::size_of_val(&self.ch_md_comment)
+            + mem::size_of_val(&self.ch_element_count)
+            + mem::size_of_val(&self.ch_type);
+
+        if !self.ch_element.is_empty() {
+            length += mem::size_of_val(&self.ch_element[0]) * self.ch_element.len();
+        }
+        length
     }
 }
